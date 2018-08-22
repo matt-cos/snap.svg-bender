6bfbfd448c5fac2374e570d9a1f97bfed6acf139
Matt Cosentino <matt@learnedmedia.com>
2018-08-21 23:31:38 -0400

    fixed the image and started on bender's antenna

--- js/main.js
+++ js/main.js
@@ -2,8 +2,23 @@
 // If you want to create new surface just provide dimensions
 // like s = Snap(800, 600);
 var s = Snap("#svg");
-// Lets create big circle in the middle:
-var button = s.circle(188, 329, 7);
+
+
+
+var antenna__stick = s.path("M208,44 L206,90 L218,90 L213,44 Z");
+antenna__stick.attr({
+	fill: "#A4B7C8",
+	stroke: "#000",
+	strokeWidth: 1.5
+});
+var antenna__ball = s.circle(210, 42, 7);
+antenna__ball.attr({
+	fill: "#D9E3EB",
+	stroke: "#000",
+	strokeWidth: 1.5
+});
+
+var button = s.circle(260, 350, 7);
 button.attr({
 	fill: "transparent",
 	stroke: "#000",
